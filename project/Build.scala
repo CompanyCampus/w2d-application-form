9fd0068f8d43ca869dccddf4f994893d6468c0a7
Alexandre Berthaud <alexandre@berthaud.me>
2013-03-22 21:39:30 +0100

    Initial commit (Play 2.1.0)

---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "w2d-appform"
+    val appVersion      = "1.0"
+
+    val appDependencies = Seq(
+      "play" %% "play-jdbc" % "2.1.0",
+      "postgresql" % "postgresql" % "9.2-1002.jdbc4",
+      "com.typesafe.slick" %% "slick" % "1.0.0",
+      "org.slf4j" % "slf4j-nop" % "1.6.4",
+      "joda-time" % "joda-time" % "2.1",
+      "org.joda" % "joda-convert" % "1.2"
+    )
+
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+      scalacOptions ++= Seq("-feature", "-deprecation", "-unchecked")
+    )
+
+}
