cd02ef6b898c69e084786ea80cffa37d1004bae9
Alexandre Berthaud <alexandre@berthaud.me>
2013-03-27 16:25:58 +0100

    Change: Presentation text

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,10 +3,5 @@ var Main = {
     $("tr").click(function(e) {
       window.location = "/records/" + e.currentTarget.dataset.uuid;
     });
-    $(".more .show").click(function(e) {
-      e.preventDefault();
-      $(".more .show").hide();
-      $(".more .content").show();
-    });
   }
 };
