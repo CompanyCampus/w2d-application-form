06e4e944155a44dc300ab3bc129d701bd944ad3a
Alexandre Berthaud <alexandre@berthaud.me>
2013-03-28 09:44:42 +0100

    Add: Date picker

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,5 +3,7 @@ var Main = {
     $("tr").click(function(e) {
       window.location = "/records/" + e.currentTarget.dataset.uuid;
     });
+    $("#companyCreation").datepicker();
+    $("#companyCreation").datepicker("option", "dateFormat", "yy-mm-dd");
   }
 };
