7804106369775388cb802d781d66f808b394f0bc
Alexandre Berthaud <alexandre@berthaud.me>
2013-03-28 16:00:51 +0100

    Change: Datepicker

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,13 @@ var Main = {
     $("tr").click(function(e) {
       window.location = "/records/" + e.currentTarget.dataset.uuid;
     });
-    $("#companyCreation").datepicker();
+    $("#companyCreation").datepicker({
+      dateFormat: "yy-mm-dd",
+      changeMonth: true,
+      changeYear: true,
+      maxDate: 0,
+      minDate: "-10Y"
+    });
     $("#companyCreation").datepicker("option", "dateFormat", "yy-mm-dd");
   }
 };
